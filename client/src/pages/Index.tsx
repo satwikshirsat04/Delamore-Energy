5ec90256f5a9d87ba0399c6c50dcc183c6671d4c
Satwik Shirsat <161075149+satwikshirsat04@users.noreply.github.com>
2025-07-16 00:09:42 +0530

    Update Index.tsx

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -85,7 +85,7 @@ const Index = () => {
         <div className="py-10 bg-black">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <h2 className="text-3xl font-extrabold text-gray-400 text-center mb-9">
-              Our Journey
+              Our Process
             </h2>
             <div className="relative w-full aspect-video overflow-hidden rounded-lg shadow-xl mt-5">
               <Carousel images={[
