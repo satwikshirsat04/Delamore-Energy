d3440629b4982c482d60b9202a5afe4fe50561be
Satwik Shirsat <161075149+satwikshirsat04@users.noreply.github.com>
2025-07-19 12:56:56 +0530

    Update Index.tsx

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -793,7 +793,7 @@ const Index = () => {
 
             </div>
 
-            {/* <div className="mt-16 text-center px-4">
+            <div className="mt-16 text-center px-4">
               <a
                 href="https://sdgs.un.org/goals"
                 target="_blank"
@@ -805,7 +805,7 @@ const Index = () => {
                   <path fillRule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
                 </svg>
               </a>
-            </div> */}
+            </div>
           </div>
         </div>
       </FadeUp>
